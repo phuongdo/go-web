88c346516fecf2c163bca9447ebe39809ae68585
phuong <phuongdoviet@admicro.vn>
2016-11-09 02:53:14 +0700

    first commit

---
+++ cronjob/sched.go
@@ -0,0 +1,23 @@
+package main
+// CronJob schedule  https://godoc.org/github.com/robfig/cron
+import (
+	//"fmt"
+	"log"
+	"time"
+	"github.com/jasonlvhit/gocron"
+)
+
+func fireAnAction() {
+	log.Println("Fire...")
+
+}
+
+const ONE_SECOND = 1 * time.Second + 10 * time.Millisecond
+
+func main() {
+	gocron.Every(1).Second().Do(fireAnAction)
+	// function Start start all the pending jobs
+	<- gocron.Start()
+
+}
+
